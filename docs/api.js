b4ada580980a11c2009aca18525b3f66a6d395dd
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-01-24 17:23:31 +0200

    docs

--- docs/api.js
+++ docs/api.js
@@ -2,7 +2,11 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Core",
-        "System"
+        "Scene",
+        "Storage",
+        "System",
+        "Timer",
+        "Tween"
     ],
     "modules": [
         "game"
