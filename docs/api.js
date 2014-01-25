5881063eeac79080f5dc271e91843b22102da871
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-01-25 18:15:09 +0200

    docs namespace

--- docs/api.js
+++ docs/api.js
@@ -1,33 +1,92 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "Body",
-        "Circle",
-        "Class",
-        "CollisionSolver",
-        "Core",
-        "Debug",
-        "Line",
-        "Loader",
-        "Pool",
-        "Rectangle",
-        "Scene",
-        "SoundManager",
-        "Sprite",
-        "Storage",
-        "System",
-        "Timer",
-        "Tween",
-        "Vector",
-        "World"
+        "game.Body",
+        "game.Circle",
+        "game.Class",
+        "game.CollisionSolver",
+        "game.Core",
+        "game.Debug",
+        "game.Line",
+        "game.Loader",
+        "game.Pool",
+        "game.Rectangle",
+        "game.Scene",
+        "game.SoundManager",
+        "game.Sprite",
+        "game.Storage",
+        "game.System",
+        "game.Timer",
+        "game.Tween",
+        "game.Vector",
+        "game.World"
     ],
     "modules": [
-        "game"
+        "core",
+        "debug",
+        "loader",
+        "physics",
+        "pool",
+        "scene",
+        "sound",
+        "sprite",
+        "storage",
+        "system",
+        "timer",
+        "tween"
     ],
     "allModules": [
         {
-            "displayName": "game",
-            "name": "game"
+            "displayName": "core",
+            "name": "core"
+        },
+        {
+            "displayName": "debug",
+            "name": "debug"
+        },
+        {
+            "displayName": "loader",
+            "name": "loader"
+        },
+        {
+            "displayName": "physics",
+            "name": "physics",
+            "description": "Physics engine."
+        },
+        {
+            "displayName": "pool",
+            "name": "pool",
+            "description": "Object pooling."
+        },
+        {
+            "displayName": "scene",
+            "name": "scene"
+        },
+        {
+            "displayName": "sound",
+            "name": "sound"
+        },
+        {
+            "displayName": "sprite",
+            "name": "sprite"
+        },
+        {
+            "displayName": "storage",
+            "name": "storage",
+            "description": "Local storage."
+        },
+        {
+            "displayName": "system",
+            "name": "system"
+        },
+        {
+            "displayName": "timer",
+            "name": "timer"
+        },
+        {
+            "displayName": "tween",
+            "name": "tween",
+            "description": "Tween engine."
         }
     ]
 } };
