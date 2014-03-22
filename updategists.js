bad4b69c1260bc83bd2c4549ee2c9ef6d804c7e8
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-03-22 16:05:49 +0200

    Site update

--- updategists.js
+++ updategists.js
@@ -1,8 +1,10 @@
 var github = require('github-basic');
 var fs = require('fs');
 
+console.log('Getting gists...');
+
 github.json('GET', '/users/:user/gists', {user: 'ekelokorpi'}, function (err, res) {
-    if(err) throw('Error getting gists');
+    if(err) return console.log('Error getting gists');
     
     var gists = res.body;
     var data = '';
@@ -14,13 +16,16 @@ github.json('GET', '/users/:user/gists', {user: 'ekelokorpi'}, function (err, re
 
         gistFile = '---\nlayout: default\ntitle: Code snippet\nheader: ' + gists[i].description + '\n---\n<script src="' + gists[i].html_url + '.js"></script>';
 
-        fs.writeFile('snippets/' + gists[i].id + '.html', gistFile, function (err) {
-            if(err) throw('Error writing gist file');
-            console.log('Gist html created.');
-        });
+        writeFile('snippets/' + gists[i].id + '.html', gistFile);
     }
-    fs.writeFile('_includes/snippets.html', data, function (err) {
-        if(err) throw('Error writing snippets file');
-        console.log('Gists updated.');
-    });
+
+    writeFile('_includes/snippets.html', data);
 });
+
+var writeFile = function(filename, data) {
+    console.log('Writing ' + filename);
+
+    fs.writeFile(filename, data, function (err) {
+        if(err) console.log('Error writing ' + filename);
+    });
+};
