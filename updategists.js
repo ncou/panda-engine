824e6e05752466fa3173e8626fe305945c47a7e8
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-03-25 11:00:15 +0200

    Site update

--- updategists.js
+++ updategists.js
@@ -7,54 +7,75 @@ for (var i = 0; i < categories.length; i++) {
     categoryData[categories[i]] = [];
 }
 
-console.log('Getting gists...');
+var perpage = 30;
+var pages = 0;
+var currentPage = 1;
 
-github.json('GET', '/users/:user/gists', {user: 'ekelokorpi'}, function (err, res) {
-    if(err) return console.log('Error getting gists');
-    
-    var gists = res.body;
-    var data = '';
-    var gistFile;
-    var category;
-    var description;
-    var last;
-    var temp;
-    for (var i = 0; i < gists.length; i++) {
-        if(!gists[i].description) continue;
+console.log('Getting gist count...');
+github.json('GET', '/users/:user', {user: 'ekelokorpi'}, function(err, res) {
+    if(err) return console.log('Error');
 
-        temp = gists[i].description.split(' ');
+    var gistCount = res.body.public_gists;
+    console.log(gistCount + ' gists found.');
+    pages = Math.ceil(gistCount / perpage);
 
-        category = temp[0];
-        last = category.substr(category.length - 1); // get last character
-        category = category.substr(0, category.length - 1); // remove last character
-
-        if(last === ':') {
-            if(categories.indexOf(category) === -1) category = 'Other';
-            temp.shift();
-            description = temp.join(' ');
-        } else {
-            category = 'Other';
-            description = gists[i].description;
-        }
-
-        categoryData[category].push([gists[i].id, description]);
-
-        gistFile = '---\nlayout: default\ntitle: Code snippet\nheader: ' + (category + ' - ' + description) + '\n---\n<script src="' + gists[i].html_url + '.js"></script>';
-
-        writeFile('snippets/' + gists[i].id + '.html', gistFile);
-    }
-
-    for(var name in categoryData) {
-        if(categoryData[name].length == 0) continue;
-
-        data += '<h6>' + name + '</h6>';
-        for (var i = 0; i < categoryData[name].length; i++) {
-            data += '<a href="/snippets/' + categoryData[name][i][0] + '.html" class="box">' + categoryData[name][i][1] + '</a>\n';
-        }
-    }
-
-    writeFile('_includes/snippets.html', data);
+    getGists();
 });
+
+var totalGistData = '';
+
+var getGists = function() {
+    console.log('Getting gists page ' + currentPage + '...');
+
+    github.json('GET', '/users/:user/gists?page='+currentPage+'&per_page='+perpage, {user: 'ekelokorpi'}, function (err, res) {
+        if(err) return console.log('Error');
+        
+        var gists = res.body;
+        var gistFile;
+        var category;
+        var description;
+        var last;
+        var temp;
+        for (var i = 0; i < gists.length; i++) {
+            if(!gists[i].description) continue;
+
+            temp = gists[i].description.split(' ');
+
+            category = temp[0];
+            last = category.substr(category.length - 1); // get last character
+            category = category.substr(0, category.length - 1); // remove last character
+
+            if(last === ':') {
+                if(categories.indexOf(category) === -1) category = 'Other';
+                temp.shift();
+                description = temp.join(' ');
+            } else {
+                category = 'Other';
+                description = gists[i].description;
+            }
+
+            categoryData[category].push([gists[i].id, description]);
+
+            gistFile = '---\nlayout: default\ntitle: Code snippet\nheader: ' + (category + ' - ' + description) + '\n---\n<script src="' + gists[i].html_url + '.js"></script>';
+
+            writeFile('snippets/' + gists[i].id + '.html', gistFile);
+        }
+
+        currentPage++;
+        if(currentPage > pages) {
+            for(var name in categoryData) {
+                if(categoryData[name].length == 0) continue;
+
+                totalGistData += '<h6>' + name + '</h6>\n';
+                for (var i = 0; i < categoryData[name].length; i++) {
+                    totalGistData += '<a href="/snippets/' + categoryData[name][i][0] + '.html" class="box">' + categoryData[name][i][1] + '</a>\n';
+                }
+            }
+            
+            writeFile('_includes/snippets.html', totalGistData);
+        } else getGists();
+    });
+};
 
 var writeFile = function(filename, data) {
     console.log('Writing ' + filename);
