c6336a1a2032533f3f6323da8f6afbf0c698683b
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-10-01 14:59:51 +0300

    Site update

---
+++ bamboo/docs/api.js
@@ -0,0 +1,28 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "bamboo.Core",
+        "bamboo.Node",
+        "bamboo.World"
+    ],
+    "modules": [
+        "bamboo",
+        "node",
+        "world"
+    ],
+    "allModules": [
+        {
+            "displayName": "bamboo",
+            "name": "bamboo"
+        },
+        {
+            "displayName": "node",
+            "name": "node"
+        },
+        {
+            "displayName": "world",
+            "name": "world"
+        }
+    ]
+} };
+});
