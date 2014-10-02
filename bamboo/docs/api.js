a298d919b63f37d2adf4c50cecfe2fde4307b480
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-10-02 12:18:20 +0300

    Site update

--- bamboo/docs/api.js
+++ bamboo/docs/api.js
@@ -1,28 +1,25 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "bamboo.Core",
+        "bamboo",
         "bamboo.Node",
+        "bamboo.Nodes.Audio",
+        "bamboo.Nodes.Camera",
+        "bamboo.Nodes.CollisionTile",
+        "bamboo.Nodes.Emitter",
+        "bamboo.Nodes.Image",
+        "bamboo.Nodes.Layer",
+        "bamboo.Nodes.Path",
+        "bamboo.Nodes.PathFollower",
+        "bamboo.Nodes.Physics",
+        "bamboo.Nodes.Rotator",
+        "bamboo.Nodes.Spine",
+        "bamboo.Nodes.Tile",
+        "bamboo.Nodes.Trigger",
+        "bamboo.Nodes.TriggerImage",
         "bamboo.World"
     ],
-    "modules": [
-        "bamboo",
-        "node",
-        "world"
-    ],
-    "allModules": [
-        {
-            "displayName": "bamboo",
-            "name": "bamboo"
-        },
-        {
-            "displayName": "node",
-            "name": "node"
-        },
-        {
-            "displayName": "world",
-            "name": "world"
-        }
-    ]
+    "modules": [],
+    "allModules": []
 } };
 });
