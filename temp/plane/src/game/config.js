530d2158661d8bffa17dd058563bc2ef08c1cba3
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-11-14 12:45:07 +0200

    Site update

---
+++ temp/plane/src/game/config.js
@@ -0,0 +1,12 @@
+pandaConfig = {
+    name: 'MyPandaGame',
+    version: '0.0.0',
+
+    system: {
+        width: 1280,
+        height: 720,
+        scaleToFit: false,
+        resizeToFill: false,
+        webGL: false
+    }
+};
